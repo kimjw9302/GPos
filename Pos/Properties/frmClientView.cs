5d59d0b9d32f61663f206266e586a3fbfa578fa2
재웅\김재웅 <jaewoong9302@gmail.com>
2018-01-04 17:03:04 +0900

    통합조회 - 판매조회, 고객조회, 매출현황 조회 화면 설계

---
+++ Pos/Properties/frmClientView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pos.Properties
+{
+    public partial class frmClientView : Form
+    {
+        public frmClientView()
+        {
+            InitializeComponent();
+        }
+    }
+}
