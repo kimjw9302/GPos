6928aed99c5e008bf12c9d7d346bc287b121597c
ach <abc@abc.com>
2018-01-19 18:19:57 +0900

    이벤트 수정

--- Pos/frmEvent.Designer.cs
+++ Pos/frmEvent.Designer.cs
@@ -30,16 +30,20 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.button4 = new System.Windows.Forms.Button();
-            this.btnDel = new System.Windows.Forms.Button();
+            this.lblDiscount = new System.Windows.Forms.Label();
+            this.lblContent = new System.Windows.Forms.Label();
             this.btnSearch = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.tboxBarcode = new System.Windows.Forms.TextBox();
+            this.lblBarcode = new System.Windows.Forms.Label();
+            this.lblStartDate = new System.Windows.Forms.Label();
+            this.lblEndDate = new System.Windows.Forms.Label();
+            this.tboxContent = new System.Windows.Forms.TextBox();
+            this.tboxDiscount = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.StartDate = new System.Windows.Forms.DateTimePicker();
+            this.EndDate = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -47,145 +51,196 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 217);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 252);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(734, 406);
+            this.dataGridView1.Size = new System.Drawing.Size(734, 339);
             this.dataGridView1.TabIndex = 0;
+            
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.textBox3);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.button4);
-            this.panel1.Controls.Add(this.btnDel);
+            this.panel1.Controls.Add(this.EndDate);
+            this.panel1.Controls.Add(this.btnModify);
+            this.panel1.Controls.Add(this.StartDate);
+            this.panel1.Controls.Add(this.btnDelete);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.tboxDiscount);
+            this.panel1.Controls.Add(this.tboxContent);
+            this.panel1.Controls.Add(this.lblEndDate);
+            this.panel1.Controls.Add(this.lblStartDate);
+            this.panel1.Controls.Add(this.lblDiscount);
+            this.panel1.Controls.Add(this.lblContent);
             this.panel1.Controls.Add(this.btnSearch);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(24, 28);
+            this.panel1.Controls.Add(this.tboxBarcode);
+            this.panel1.Controls.Add(this.lblBarcode);
+            this.panel1.Location = new System.Drawing.Point(12, 11);
             this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(734, 142);
+            this.panel1.Size = new System.Drawing.Size(734, 236);
             this.panel1.TabIndex = 4;
             // 
-            // textBox3
+            // lblDiscount
             // 
-            this.textBox3.Font = new System.Drawing.Font("굴림", 18F);
-            this.textBox3.Location = new System.Drawing.Point(173, 95);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(172, 35);
-            this.textBox3.TabIndex = 12;
+            this.lblDiscount.AutoSize = true;
+            this.lblDiscount.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblDiscount.Location = new System.Drawing.Point(57, 91);
+            this.lblDiscount.Name = "lblDiscount";
+            this.lblDiscount.Size = new System.Drawing.Size(85, 24);
+            this.lblDiscount.TabIndex = 11;
+            this.lblDiscount.Text = "할인율";
             // 
-            // label2
+            // lblContent
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label2.Location = new System.Drawing.Point(52, 99);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(110, 24);
-            this.label2.TabIndex = 11;
-            this.label2.Text = "전화번호";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Font = new System.Drawing.Font("굴림", 18F);
-            this.textBox2.Location = new System.Drawing.Point(173, 52);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(172, 35);
-            this.textBox2.TabIndex = 10;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label4.Location = new System.Drawing.Point(82, 58);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 24);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "이름";
-            // 
-            // button4
-            // 
-            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(37)))), ((int)(((byte)(38)))));
-            this.button4.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold);
-            this.button4.ForeColor = System.Drawing.Color.White;
-            this.button4.Location = new System.Drawing.Point(537, 6);
-            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(182, 62);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "수정";
-            this.button4.UseVisualStyleBackColor = false;
-            // 
-            // btnDel
-            // 
-            this.btnDel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(37)))), ((int)(((byte)(38)))));
-            this.btnDel.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold);
-            this.btnDel.ForeColor = System.Drawing.Color.White;
-            this.btnDel.Location = new System.Drawing.Point(537, 75);
-            this.btnDel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnDel.Name = "btnDel";
-            this.btnDel.Size = new System.Drawing.Size(182, 52);
-            this.btnDel.TabIndex = 1;
-            this.btnDel.Text = "삭제";
-            this.btnDel.UseVisualStyleBackColor = false;
+            this.lblContent.AutoSize = true;
+            this.lblContent.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblContent.Location = new System.Drawing.Point(82, 52);
+            this.lblContent.Name = "lblContent";
+            this.lblContent.Size = new System.Drawing.Size(60, 24);
+            this.lblContent.TabIndex = 9;
+            this.lblContent.Text = "내용";
             // 
             // btnSearch
             // 
             this.btnSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(37)))), ((int)(((byte)(38)))));
             this.btnSearch.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold);
             this.btnSearch.ForeColor = System.Drawing.Color.White;
-            this.btnSearch.Location = new System.Drawing.Point(407, 6);
+            this.btnSearch.Location = new System.Drawing.Point(456, 6);
             this.btnSearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(125, 122);
+            this.btnSearch.Size = new System.Drawing.Size(130, 110);
             this.btnSearch.TabIndex = 3;
             this.btnSearch.Text = "조회";
             this.btnSearch.UseVisualStyleBackColor = false;
             // 
-            // textBox1
+            // tboxBarcode
             // 
-            this.textBox1.Font = new System.Drawing.Font("굴림", 18F);
-            this.textBox1.Location = new System.Drawing.Point(173, 10);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(172, 35);
-            this.textBox1.TabIndex = 2;
+            this.tboxBarcode.Font = new System.Drawing.Font("굴림", 18F);
+            this.tboxBarcode.Location = new System.Drawing.Point(148, 10);
+            this.tboxBarcode.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tboxBarcode.Name = "tboxBarcode";
+            this.tboxBarcode.Size = new System.Drawing.Size(302, 35);
+            this.tboxBarcode.TabIndex = 2;
             // 
-            // label1
+            // lblBarcode
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(60, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 24);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "고객번호";
+            this.lblBarcode.AutoSize = true;
+            this.lblBarcode.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblBarcode.Location = new System.Drawing.Point(57, 15);
+            this.lblBarcode.Name = "lblBarcode";
+            this.lblBarcode.Size = new System.Drawing.Size(85, 24);
+            this.lblBarcode.TabIndex = 0;
+            this.lblBarcode.Text = "바코드";
             // 
-            // button1
+            // lblStartDate
             // 
-            this.button1.Location = new System.Drawing.Point(584, 188);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(159, 23);
-            this.button1.TabIndex = 13;
-            this.button1.Text = "이벤트 추가하기";
-            this.button1.UseVisualStyleBackColor = true;
+            this.lblStartDate.AutoSize = true;
+            this.lblStartDate.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblStartDate.Location = new System.Drawing.Point(32, 132);
+            this.lblStartDate.Name = "lblStartDate";
+            this.lblStartDate.Size = new System.Drawing.Size(110, 24);
+            this.lblStartDate.TabIndex = 13;
+            this.lblStartDate.Text = "시작날짜";
+            // 
+            // lblEndDate
+            // 
+            this.lblEndDate.AutoSize = true;
+            this.lblEndDate.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblEndDate.Location = new System.Drawing.Point(7, 173);
+            this.lblEndDate.Name = "lblEndDate";
+            this.lblEndDate.Size = new System.Drawing.Size(135, 24);
+            this.lblEndDate.TabIndex = 14;
+            this.lblEndDate.Text = "끝나는날짜";
+            // 
+            // tboxContent
+            // 
+            this.tboxContent.Font = new System.Drawing.Font("굴림", 18F);
+            this.tboxContent.Location = new System.Drawing.Point(148, 49);
+            this.tboxContent.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tboxContent.Name = "tboxContent";
+            this.tboxContent.Size = new System.Drawing.Size(302, 35);
+            this.tboxContent.TabIndex = 15;
+            // 
+            // tboxDiscount
+            // 
+            this.tboxDiscount.Font = new System.Drawing.Font("굴림", 18F);
+            this.tboxDiscount.Location = new System.Drawing.Point(148, 88);
+            this.tboxDiscount.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tboxDiscount.Name = "tboxDiscount";
+            this.tboxDiscount.Size = new System.Drawing.Size(302, 35);
+            this.tboxDiscount.TabIndex = 16;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(37)))), ((int)(((byte)(38)))));
+            this.btnAdd.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold);
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(456, 120);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(130, 110);
+            this.btnAdd.TabIndex = 19;
+            this.btnAdd.Text = "추가";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(37)))), ((int)(((byte)(38)))));
+            this.btnDelete.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold);
+            this.btnDelete.ForeColor = System.Drawing.Color.White;
+            this.btnDelete.Location = new System.Drawing.Point(592, 120);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(130, 110);
+            this.btnDelete.TabIndex = 20;
+            this.btnDelete.Text = "삭제";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnModify
+            // 
+            this.btnModify.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(37)))), ((int)(((byte)(38)))));
+            this.btnModify.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold);
+            this.btnModify.ForeColor = System.Drawing.Color.White;
+            this.btnModify.Location = new System.Drawing.Point(592, 6);
+            this.btnModify.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(130, 110);
+            this.btnModify.TabIndex = 21;
+            this.btnModify.Text = "수정";
+            this.btnModify.UseVisualStyleBackColor = false;
+            this.btnModify.Click += new System.EventHandler(this.btnModify_Click);
+            // 
+            // StartDate
+            // 
+            this.StartDate.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.StartDate.Location = new System.Drawing.Point(148, 127);
+            this.StartDate.Name = "StartDate";
+            this.StartDate.Size = new System.Drawing.Size(302, 35);
+            this.StartDate.TabIndex = 22;
+            // 
+            // EndDate
+            // 
+            this.EndDate.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.EndDate.Location = new System.Drawing.Point(148, 168);
+            this.EndDate.Name = "EndDate";
+            this.EndDate.Size = new System.Drawing.Size(302, 35);
+            this.EndDate.TabIndex = 23;
+            this.EndDate.Value = new System.DateTime(2018, 1, 31, 0, 0, 0, 0);
             // 
             // frmEvent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(770, 603);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(757, 603);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "frmEvent";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmEvent";
+            this.Load += new System.EventHandler(this.frmEvent_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -197,15 +252,19 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Label lblDiscount;
+        private System.Windows.Forms.Label lblContent;
         private System.Windows.Forms.Button btnSearch;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tboxBarcode;
+        private System.Windows.Forms.Label lblBarcode;
+        private System.Windows.Forms.DateTimePicker EndDate;
+        private System.Windows.Forms.Button btnModify;
+        private System.Windows.Forms.DateTimePicker StartDate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox tboxDiscount;
+        private System.Windows.Forms.TextBox tboxContent;
+        private System.Windows.Forms.Label lblEndDate;
+        private System.Windows.Forms.Label lblStartDate;
     }
 }
--- Pos/frmEvent.cs
+++ Pos/frmEvent.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +13,179 @@ using System.Windows.Forms;
 
 namespace Pos
 {
-    public partial class frmEvent : Form,ISearch,IDelete,IAlter
+    public partial class frmEvent : Form
     {
+        DBcontroller con;
+        DataSet ds;
         public frmEvent()
         {
             InitializeComponent();
+        }
+
+        private void Rests()
+        {
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["gposStr"].ConnectionString))
+            {
+                using (var cmd = new SqlCommand("EventLoad", con))
+                {
+                    con.Open();
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    SqlDataAdapter adapter = new SqlDataAdapter();
+                    adapter.SelectCommand = cmd;
+                    con.Close();
+
+                    ds = new DataSet();
+                    adapter.Fill(ds);
+                    this.dataGridView1.DataSource = ds.Tables[0];
+
+                    dataGridView1.Columns[0].HeaderText = "바코드";
+                    dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                    dataGridView1.Columns[1].HeaderText = "내용";
+                    dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                    dataGridView1.Columns[2].HeaderText = "할인율";
+                    dataGridView1.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                    dataGridView1.Columns[3].HeaderText = "시작날짜";
+                    dataGridView1.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                    dataGridView1.Columns[4].HeaderText = "끝나는날짜";
+                    dataGridView1.Columns[4].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                }
+            }
+        }
+
+
+        private void frmEvent_Load(object sender, EventArgs e)
+        {
+            Rests();
+        }
+
+        // 저장
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string uBarcode = this.tboxBarcode.Text.Trim();
+            string uEventContent = this.tboxContent.Text.Trim();
+            string uDiscount = this.tboxDiscount.Text.Trim();
+            string uStartDate = this.StartDate.Value.ToLongDateString();
+            string uEndDate = this.EndDate.Value.ToLongDateString();
+
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["gposStr"].ConnectionString))
+            {
+                using (var cmd = new SqlCommand("EventSave", con))
+                {
+                    cmd.Parameters.AddWithValue("@uBarcode", uBarcode);
+                    cmd.Parameters.AddWithValue("@uEventContent", uEventContent);
+                    cmd.Parameters.AddWithValue("@uDiscount", uDiscount);
+                    cmd.Parameters.AddWithValue("@uStartDate", uStartDate);
+                    cmd.Parameters.AddWithValue("@uEndDate", uEndDate);
+
+
+
+                    con.Open();
+
+                    int i = cmd.ExecuteNonQuery();
+                    if (i == 1)
+                    {
+                        MessageBox.Show("저장 되었습니다.");
+                        Rests();
+                        return;
+                    }
+                    else
+                    {
+                        MessageBox.Show("저장 실패");
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void ComponentInit()
+        {
+            tboxBarcode.Text = tboxContent.Text = tboxDiscount.Text = StartDate.Text = EndDate.Text = "";
+        }
+
+        // 수정
+        private void btnModify_Click(object sender, EventArgs e)
+        {
+            string uBarcode = this.tboxBarcode.Text.Trim();
+            string uEventContent = this.tboxContent.Text.Trim();
+            string uDiscount = this.tboxDiscount.Text.Trim();
+            string uStartDate = this.StartDate.Value.ToLongDateString();
+            string uEndDate = this.EndDate.Value.ToLongDateString();
+
+
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["gposStr"].ConnectionString))
+            {
+                using (var cmd = new SqlCommand("EventsModify", con))
+                {
+                    con.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter();
+
+                    cmd.Parameters.AddWithValue("@uBarcode", uBarcode);
+                    cmd.Parameters.AddWithValue("@uEventContent", uEventContent);
+                    cmd.Parameters.AddWithValue("@uDiscount", uDiscount);
+                    cmd.Parameters.AddWithValue("@uStartDate", uStartDate);
+                    cmd.Parameters.AddWithValue("@uEndDate", uEndDate);
+
+                    adapter.UpdateCommand = cmd;
+
+                    int i;
+                    i = cmd.ExecuteNonQuery();
+
+                    if (i == 1)
+                    {
+                        MessageBox.Show("저장 되었습니다.");
+                        Rests();
+                        ComponentInit();
+                        return;
+                    }
+                    else
+                    {
+                        MessageBox.Show("저장 실패");
+                        return;
+                    }
+                }
+            }
+        }
+    
+
+        // 삭제
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string uBarcode = this.tboxBarcode.Text.Trim();
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["gposStr"].ConnectionString))
+            {
+                using (var cmd = new SqlCommand("EventDelete", con))
+                {
+                    con.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter();
+                    cmd.Parameters.AddWithValue("@uBarcode", uBarcode);
+
+                    adapter.DeleteCommand = cmd;
+
+                    int i = cmd.ExecuteNonQuery();
+                    if (i == 1)
+                    {
+                        MessageBox.Show("삭제 되었습니다.");
+                        Rests();
+                        return;
+                    }
+                    else
+                    {
+                        MessageBox.Show("삭제 실패");
+
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            tboxBarcode.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            tboxContent.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            tboxDiscount.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            StartDate.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            EndDate.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
         }
     }
 }
