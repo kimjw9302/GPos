1bba29d4c7812b88864e0f3b07c5ee28abdd475f
jun <avc@vas.com>
2018-01-22 16:00:51 +0900

    매출 현황 조회 폼 커밋

--- Pos/frmRevenue.Designer.cs
+++ Pos/frmRevenue.Designer.cs
@@ -28,13 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.dtStart = new System.Windows.Forms.DateTimePicker();
+            this.dtEnd = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.panel = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // listBox1
@@ -43,55 +43,89 @@
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 17;
             this.listBox1.Items.AddRange(new object[] {
-            "1. 시간대별 매출현황 ",
-            "2. 카테고리별 매출현황 ",
-            "3. 연령대별 매출현황 ",
-            "4. 결제수단별 매출현황 ",
-            "5. 물품별 매출 현황",
-            "6. 매출취소현황",
-            "7. 매장 총 매출"});
-            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            "1. 시간대별 매출현황",
+            "2. 카테고리별 매출현황",
+            "3. 상품별 매출현황",
+            "4. 객층분석",
+            "5. 결제수단별 매출현황",
+            "6. 반품현황",
+            "7. 총 매출",
+            "8. 실 매출"});
+            this.listBox1.Location = new System.Drawing.Point(23, 87);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(272, 650);
+            this.listBox1.Size = new System.Drawing.Size(272, 582);
             this.listBox1.TabIndex = 0;
             // 
-            // chart1
+            // dtStart
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(317, 24);
-            this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(427, 319);
-            this.chart1.TabIndex = 1;
-            this.chart1.Text = "chart1";
+            this.dtStart.Font = new System.Drawing.Font("맑은 고딕", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.dtStart.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtStart.Location = new System.Drawing.Point(146, 30);
+            this.dtStart.Name = "dtStart";
+            this.dtStart.Size = new System.Drawing.Size(177, 30);
+            this.dtStart.TabIndex = 3;
             // 
-            // label1
+            // dtEnd
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(894, 42);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 12);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.dtEnd.Font = new System.Drawing.Font("맑은 고딕", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.dtEnd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtEnd.Location = new System.Drawing.Point(368, 30);
+            this.dtEnd.Name = "dtEnd";
+            this.dtEnd.Size = new System.Drawing.Size(200, 30);
+            this.dtEnd.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(53, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "조회기간";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(335, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "~";
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(608, 25);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(108, 51);
+            this.btnOk.TabIndex = 7;
+            this.btnOk.Text = "조회";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(301, 87);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(756, 582);
+            this.panel.TabIndex = 8;
             // 
             // frmRevenue
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(1087, 707);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtEnd);
+            this.Controls.Add(this.dtStart);
             this.Controls.Add(this.listBox1);
             this.Name = "frmRevenue";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmRevenue";
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,7 +134,11 @@
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtStart;
+        private System.Windows.Forms.DateTimePicker dtEnd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Panel panel;
     }
 }
