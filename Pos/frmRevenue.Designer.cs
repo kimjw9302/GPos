532dec248d9d85810e2a23e6f8084c87dff0b926
kojihye <kojihye118@gmail.com>
2018-01-25 18:22:08 +0900

    매출현황 성별, 연령별 날짜변경시 바뀌게 변경 완료

--- Pos/frmRevenue.Designer.cs
+++ Pos/frmRevenue.Designer.cs
@@ -28,12 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint13 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 0D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint14 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 0D);
+            System.Windows.Forms.DataVisualization.Charting.Title title3 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint15 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 0D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint16 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 0D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint17 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 0D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint18 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 0D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint19 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 0D);
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint20 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 0D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint21 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 0D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint22 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 0D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint23 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 0D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint24 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 0D);
+            System.Windows.Forms.DataVisualization.Charting.Title title4 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.dtStart = new System.Windows.Forms.DateTimePicker();
             this.dtEnd = new System.Windows.Forms.DateTimePicker();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.btnOk = new System.Windows.Forms.Button();
+            this.chartGender = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartAge = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chartGender)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartAge)).BeginInit();
             this.SuspendLayout();
             // 
             // listBox1
@@ -54,7 +79,6 @@
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(272, 582);
             this.listBox1.TabIndex = 0;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // dtStart
             // 
@@ -102,6 +126,92 @@
             this.btnOk.TabIndex = 7;
             this.btnOk.Text = "조회";
             this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click_1);
+            // 
+            // chartGender
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chartGender.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chartGender.Legends.Add(legend3);
+            this.chartGender.Location = new System.Drawing.Point(760, 87);
+            this.chartGender.Name = "chartGender";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series4.Legend = "Legend1";
+            series4.Name = "Gender";
+            dataPoint13.AxisLabel = "남자";
+            dataPoint13.IsValueShownAsLabel = true;
+            dataPoint13.LabelFormat = "{0.0}%";
+            dataPoint14.AxisLabel = "여자";
+            dataPoint14.IsValueShownAsLabel = true;
+            dataPoint14.LabelFormat = "{0.0}%";
+            series4.Points.Add(dataPoint13);
+            series4.Points.Add(dataPoint14);
+            this.chartGender.Series.Add(series4);
+            this.chartGender.Size = new System.Drawing.Size(315, 273);
+            this.chartGender.TabIndex = 8;
+            this.chartGender.Text = "chart1";
+            title3.Alignment = System.Drawing.ContentAlignment.MiddleLeft;
+            title3.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title3.Name = "Title1";
+            title3.Text = "<< 성별 매출현황조회 >>";
+            title3.TextStyle = System.Windows.Forms.DataVisualization.Charting.TextStyle.Shadow;
+            this.chartGender.Titles.Add(title3);
+            this.chartGender.Visible = false;
+            // 
+            // chartAge
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chartAge.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chartAge.Legends.Add(legend4);
+            this.chartAge.Location = new System.Drawing.Point(301, 87);
+            this.chartAge.Name = "chartAge";
+            series5.ChartArea = "ChartArea1";
+            series5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series5.IsValueShownAsLabel = true;
+            series5.Legend = "Legend1";
+            series5.Name = "남자";
+            dataPoint15.LabelFormat = "{0.0}%";
+            dataPoint16.LabelFormat = "{0.0}%";
+            dataPoint17.LabelFormat = "{0.0}%";
+            dataPoint18.LabelFormat = "{0.0}%";
+            dataPoint19.IsValueShownAsLabel = false;
+            dataPoint19.LabelFormat = "{0.0}%";
+            series5.Points.Add(dataPoint15);
+            series5.Points.Add(dataPoint16);
+            series5.Points.Add(dataPoint17);
+            series5.Points.Add(dataPoint18);
+            series5.Points.Add(dataPoint19);
+            series5.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.String;
+            series6.ChartArea = "ChartArea1";
+            series6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series6.IsValueShownAsLabel = true;
+            series6.Legend = "Legend1";
+            series6.Name = "여자";
+            dataPoint20.LabelFormat = "{0.0}%";
+            dataPoint21.LabelFormat = "{0.0}%";
+            dataPoint22.LabelFormat = "{0.0}%";
+            dataPoint23.LabelFormat = "{0.0}%";
+            dataPoint24.LabelFormat = "{0.0}%";
+            series6.Points.Add(dataPoint20);
+            series6.Points.Add(dataPoint21);
+            series6.Points.Add(dataPoint22);
+            series6.Points.Add(dataPoint23);
+            series6.Points.Add(dataPoint24);
+            series6.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.String;
+            this.chartAge.Series.Add(series5);
+            this.chartAge.Series.Add(series6);
+            this.chartAge.Size = new System.Drawing.Size(453, 582);
+            this.chartAge.TabIndex = 9;
+            this.chartAge.Text = "chart1";
+            title4.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            title4.Name = "Title1";
+            title4.Text = "<< 연령별 매출현황 조회 >>";
+            title4.TextStyle = System.Windows.Forms.DataVisualization.Charting.TextStyle.Shadow;
+            this.chartAge.Titles.Add(title4);
+            this.chartAge.Visible = false;
             // 
             // frmRevenue
             // 
@@ -109,6 +219,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(1087, 707);
+            this.Controls.Add(this.chartAge);
+            this.Controls.Add(this.chartGender);
             this.Controls.Add(this.btnOk);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -118,6 +230,8 @@
             this.Name = "frmRevenue";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmRevenue";
+            ((System.ComponentModel.ISupportInitialize)(this.chartGender)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartAge)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -131,5 +245,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartGender;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartAge;
     }
 }
--- Pos/frmRevenue.cs
+++ Pos/frmRevenue.cs
@@ -15,15 +15,11 @@ namespace Pos
     public partial class frmRevenue : Form,ISearch,IAlter
     {
         SqlConnection con;
-        //지혜 - 남녀 비율 차트 생성시 필요
-        ChartArea chartArea1 = new ChartArea();
-        Legend legend1 = new Legend();
-        Series gender = new Series();
-        DataPoint dpMan;
-        DataPoint dpWomen;
-        private Chart chart1;
-        private List<decimal> value = new List<decimal>();
-        Title CirTitle = new Title();
+        SqlDataAdapter adapter;
+        DataSet ds;
+      
+
+        #region 막대차트디자인
         //~ 지혜 - 막대차트
         DataTable manTable, womenTable;
         DataRowCollection manRow, womenRow;
@@ -35,12 +31,29 @@ namespace Pos
         ChartArea chartArea2 = new ChartArea();
         Legend legend2 = new Legend();
         private Chart chart2;
+        DataPoint m10 = new DataPoint(0, 0);
+        DataPoint m20 = new DataPoint(0, 0);
+        DataPoint m30 = new DataPoint(0, 0);
+        DataPoint m40 = new DataPoint(0, 0);
+        DataPoint m50 = new DataPoint(0, 0);
+        DataPoint w10 = new DataPoint(0, 0);
+        DataPoint w20 = new DataPoint(0, 0);
+        DataPoint w30 = new DataPoint(0, 0);
+        DataPoint w40 = new DataPoint(0, 0);
+        DataPoint w50 = new DataPoint(0, 0); 
+        #endregion
 
-        DataPoint m10, m20, m30, m40, m50, w10, w20, w30, w40, w50;
+        //private DataPointCollection dataPointM , dataPointW;
+
+        //버튼누르면
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            
+          
+        }
+
         Title BarTitle = new Title();
-        //
-
-
+        
         public frmRevenue()
         {
             InitializeComponent();
@@ -57,15 +70,14 @@ namespace Pos
                 case 2:
                     break;
                 case 3: //지혜
-                    GenderAnalysis();
-                    AgeAnalysis();
+                    GenderChart();
+                    AgeChart();
                     break;
                 case 4: 
                     break;
                 case 5:
                     break;
                 case 6: //지혜
-                    TotalRevenue();
                     break;
                 case 7:
                     break;
@@ -73,21 +85,64 @@ namespace Pos
             
         }
 
-        /*
-         *  0열 (x) - 연령대 /  1열 (y) - 비율 
-         * 10대, 20대, 30대, 40대, 50대이상  datapoint 5개 * 2 (성별)
-         * 
-         */
-        // 나이별 분석- 막대그래프
-        private void AgeAnalysis()
+
+        //조회버튼클릭
+        private void btnOk_Click_1(object sender, EventArgs e)
         {
+            switch (listBox1.SelectedIndex)
+            {
+                case 0:
+                    TimeRevenue();
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3: //지혜
+                    GenderChart(); //성별
+                    AgeChart(); //연령별
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6: 
+                    break;
+                case 7:
+                    break;
+            }
+        }
+
+        private void TimeRevenue()
+        {
+            TextBox txtTime = new TextBox();
+            txtTime.Location = new Point(319, 87);
+            txtTime.Multiline = true;
+            txtTime.Name = "txtTime";
+            txtTime.Size = new Size(435, 582);
+            this.Controls.Add(txtTime);
+
+        }
+
+        private void AgeChart()
+        {
+            chartAge.Series[0].Points.Clear();
+            chartAge.Series[1].Points.Clear();
+            chartAge.Visible = true;
             con = DBcontroller.Instance();
             using (var cmd = new SqlCommand("AgeAnalysis", con))
             {
-                con.Open();
 
-                SqlDataAdapter adapter = new SqlDataAdapter();
-                DataSet ds = new DataSet();
+                string strDate = dtStart.Value.ToShortDateString() + " 00:00:00";
+                string endDate = dtEnd.Value.ToShortDateString() + " 23:59:59";
+
+                con.Open();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@startDate", DateTime.Parse(strDate));
+                cmd.Parameters.AddWithValue("@endDate", DateTime.Parse(endDate));
+
+                adapter = new SqlDataAdapter();
+                ds = new DataSet();
                 adapter.SelectCommand = cmd;
                 adapter.Fill(ds);
                 manTable = ds.Tables[0];
@@ -95,162 +150,147 @@ namespace Pos
 
                 manRow = manTable.Rows;
                 womenRow = womenTable.Rows;
+
             }
             con.Close();
-
-
-
 
             foreach (DataRow item in manRow)
             {
-                //첫번째 : 나이, 두번째 : 비율
-                xValue.Add(item[0].ToString());
-                yValueMan.Add((decimal)item[1]);
+                switch (item[0].ToString())
+                {
+                    case "10대":
+                        m10.SetValueY(item[1]);
+                        break;
+                    case "20대":
+                        m20.SetValueY(item[1]);
+                        break;
+                    case "30대":
+                        m30.SetValueY(item[1]);
+                        break;
+                    case "40대":
+                        m40.SetValueY(item[1]);
+                        break;
+                    default:
+                        m50.SetValueY(item[1]);
+                        break;
+                }
             }
-            //남자 datapoint 추가
-            m10 = new DataPoint(0, (Double)yValueMan[0]);
-            m20 = new DataPoint(0, (Double)yValueMan[1]);
-            m30 = new DataPoint(0, (Double)yValueMan[2]);
-            m40 = new DataPoint(0, (Double)yValueMan[3]);
-            m50 = new DataPoint(0, (Double)yValueMan[4]);
-
-
             foreach (DataRow item in womenRow)
             {
-                xValue.Add(item[0].ToString());
-                yValueWom.Add((decimal)item[1]);
+                switch (item[0].ToString())
+                {
+                    case "10대":
+                        w10.SetValueY(item[1]);
+                        break;
+                    case "20대":
+                        w20.SetValueY(item[1]);
+                        break;
+                    case "30대":
+                        w30.SetValueY(item[1]);
+                        break;
+                    case "40대":
+                        w40.SetValueY(item[1]);
+                        break;
+                    default:
+                        w50.SetValueY(item[1]);
+                        break;
+                }
             }
-            //여자 datapoint 추가
-            w10 = new DataPoint(0, (Double)yValueWom[0]);
-            w20 = new DataPoint(0, (Double)yValueWom[1]);
-            w30 = new DataPoint(0, (Double)yValueWom[2]);
-            w40 = new DataPoint(0, (Double)yValueWom[3]);
-            w50 = new DataPoint(0, (Double)yValueWom[4]);
 
-
-            serMan.Font = new Font("맑은 고딕", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
-            serWom.Font = new Font("맑은 고딕", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
-          
-            serMan.IsValueShownAsLabel = true;
-            serWom.IsValueShownAsLabel = true;
-            chart2 = new Chart();
-            this.Controls.Add(this.chart2);
-
-
-            chartArea2.Name = "ChartArea2";
-            this.chart2.ChartAreas.Add(chartArea2);
+            chartAge.Series["남자"].Points.Add(m10);
+            chartAge.Series["남자"].Points.Add(m20);
+            chartAge.Series["남자"].Points.Add(m30);
+            chartAge.Series["남자"].Points.Add(m40);
+            chartAge.Series["남자"].Points.Add(m50);
+            chartAge.Series["여자"].Points.Add(w10);
+            chartAge.Series["여자"].Points.Add(w20);
+            chartAge.Series["여자"].Points.Add(w30);
+            chartAge.Series["여자"].Points.Add(w40);
+            chartAge.Series["여자"].Points.Add(w50);
+            chartAge.Series["남자"].Points[0].AxisLabel = "10대";
+            chartAge.Series["남자"].Points[1].AxisLabel = "20대";
+            chartAge.Series["남자"].Points[2].AxisLabel = "30대";
+            chartAge.Series["남자"].Points[3].AxisLabel = "40대";
+            chartAge.Series["남자"].Points[4].AxisLabel = "50대 이상";
             
-           
-            legend1.Name = "Legend2";
-            this.chart2.Legends.Add(legend2);
-            this.chart2.Location = new Point(314, 87);
-            this.chart2.Name = "chart2";
-            
-            serMan.Points.Add(m10);
-            serMan.Points.Add(m20);
-            serMan.Points.Add(m30);
-            serMan.Points.Add(m40);
-            serMan.Points.Add(m50);
 
-            serWom.Points.Add(w10);
-            serWom.Points.Add(w20);
-            serWom.Points.Add(w30);
-            serWom.Points.Add(w40);
-            serWom.Points.Add(w50);
+            chartAge.Series["남자"].Font = new Font("맑은 고딕", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+            chartAge.Series["여자"].Font = new Font("맑은 고딕", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
 
-            serMan.LegendText = "남자";
-            serWom.LegendText = "여자";
-            serMan.Color = Color.SkyBlue;
-            serWom.Color = Color.LightPink;
+            chartAge.Series["남자"].IsValueShownAsLabel = true;
+            chartAge.Series["여자"].IsValueShownAsLabel = true;
 
-            serMan.ChartArea = "ChartArea2";
-            serWom.ChartArea = "ChartArea2";
-
-
-
-            BarTitle.Alignment = ContentAlignment.TopLeft;
-            BarTitle.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(129)));
-            BarTitle.TextStyle = TextStyle.Shadow;
-            BarTitle.Text = "<< 연령별, 성별 매출현황 조회 >>";
-            chart2.Titles.Add(BarTitle);
-            chart2.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
-            chart2.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
-            //추가
-            this.chart2.Series.Add(serMan);
-            this.chart2.Series.Add(serWom);
-            
-          this.chart2.Size = new Size(478, 582);
-            this.chart2.Text = "chart2";
-
-
+            chartAge.Series["남자"].Color = Color.SkyBlue;
+            chartAge.Series["여자"].Color = Color.LightPink;
         }
 
-        //연령별 분석 - 원형
-        private void GenderAnalysis()
+        private void GenderChart()
         {
+
+            chartGender.Series["Gender"].Points.Clear();
+            chartGender.Visible = true;
             con = DBcontroller.Instance();
-            using (var cmd = new SqlCommand("MemberAgeSales",con))
+            using (var cmd = new SqlCommand("MemberAgeSales", con))
             {
+                string strDate = dtStart.Value.ToShortDateString() + " 00:00:00";
+                string endDate = dtEnd.Value.ToShortDateString() + " 23:59:59";
+
                 con.Open();
-                SqlDataReader read = cmd.ExecuteReader();
-                
-                while (read.Read())
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@startDate", DateTime.Parse(strDate));
+                cmd.Parameters.AddWithValue("@endDate", DateTime.Parse(endDate));
+
+                DataSet ds = new DataSet();
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.SelectCommand = cmd;
+                adapter.Fill(ds);
+
+                DataRowCollection rows = ds.Tables[0].Rows;
+                decimal man =0;
+                decimal woman =0;
+                foreach (var item in rows)
                 {
-                    //datapoint 값 담기
-                    value.Add(read.GetDecimal(1));
+                    if(rows.Count>1)
+                    {
+                         man = decimal.Parse(ds.Tables[0].Rows[0][1].ToString());
+                        woman = decimal.Parse(ds.Tables[0].Rows[1][1].ToString());
+                    }
                 }
                 
+                
+
+                //MethodReturnChat.Series["method"].IsValueShownAsLabel = true;
+                //MethodReturnChat.Series["method"].LabelFormat = "{0.0}%";
+                if (man == 0 && woman == 0 )
+                {
+                    chartGender.Series["Gender"].Points.AddXY("데이터값 없음", 100);
+                    chartGender.Series["Gender"].Points[0].Color = Color.Coral;
+                }
+                if (man != 0)
+                {
+                    chartGender.Series["Gender"].Points.AddXY("남자", man);
+                    chartGender.Series["Gender"].Points[0].IsValueShownAsLabel = true;
+                    chartGender.Series["Gender"].Points[0].LabelFormat = "{0.0}%";
+                    chartGender.Series["Gender"].Points[0].Color = Color.SkyBlue;
+                 
+
+                }
+                if (woman != 0)
+                {
+                    chartGender.Series["Gender"].Points.AddXY("여자", woman);
+                    chartGender.Series["Gender"].Points[1].IsValueShownAsLabel = true;
+                    chartGender.Series["Gender"].Points[1].LabelFormat = "{0.0}%";
+                    chartGender.Series["Gender"].Points[1].Color = Color.LightPink;
+                }
+
+                chartGender.Series["Gender"].Font = new Font("맑은 고딕", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+
+                con.Close();
+
+
             }
-            con.Close();
-
-            dpMan = new DataPoint(0, (double)value[0]);
-            dpWomen = new DataPoint(0, (double)value[1]);
-            dpMan.LegendText = "남자";
-            dpWomen.LegendText = "여자";
-            dpMan.IsValueShownAsLabel = true;
-            dpWomen.IsValueShownAsLabel = true;
-            dpMan.LabelFormat = "{0.0}%";
-            dpWomen.LabelFormat = "{0.0}%";
-            dpMan.Color = Color.SkyBlue;
-            dpWomen.Color = Color.LightPink;
-            
-
-            this.chart1 = new Chart();
-            this.Controls.Add(this.chart1);
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new Point(798, 436);
-            this.chart1.Name = "chart1";
-
-
-            gender.Font = new Font("맑은 고딕", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
-            gender.Points.Add(dpMan);
-            gender.Points.Add(dpWomen);
-
-            gender.ChartArea = "ChartArea1";
-            gender.Legend = "Legend1";
-            gender.Name = "man";
-            gender.ChartType = SeriesChartType.Pie;
-
-            CirTitle.Alignment = ContentAlignment.TopLeft;
-            CirTitle.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(129)));
-            CirTitle.TextStyle = TextStyle.Shadow;
-            CirTitle.Text = "<< 성별 매출현황 조회 >>";
-            chart1.Titles.Add(CirTitle);
-            //차트에 추가
-            this.chart1.Series.Add(gender);
-            
-            this.chart1.Size = new Size(267, 233);
-            this.chart1.Text = "chart1";
-           
-
         }
-        //총매출
-        private void TotalRevenue()
-        {
-            throw new NotImplementedException();
-        }
+        
+     
     }
 }
